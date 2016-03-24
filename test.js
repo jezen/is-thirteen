6ec8b2544097cff53c2ee6b13f10882fac19fee6
Caio Dias <caiodias@users.noreply.github.com>
2016-03-24 10:19:47 -0400

    Added tests for new languages
    Testing the new languages:
    - Portuguese
    - French

--- test.js
+++ test.js
@@ -9,6 +9,10 @@ tap.equal(isThirteen('13', false), true);
 
 tap.equal(isThirteen('thirteen'), true);
 tap.equal(isThirteen('Thirteen'), true);
+tap.equal(isThirteen('treze'), true);   // Portuguese
+tap.equal(isThirteen('Treze'), true);   // Portuguese
+tap.equal(isThirteen('treize'), true);  // French
+tap.equal(isThirteen('Treize'), true);  // French
 tap.equal(isThirteen('Remy Hadley'), true);
 
 tap.equal(isThirteen('https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/s320x320/e35/12237511_444845689040315_1101385461_n.jpg'), true);
