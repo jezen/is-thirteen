4a97811afc265a57db453f1217f429c735f5e7f1
Benjamin Gruenbaum <inglor@gmail.com>
2016-03-24 16:44:11 +0200

    Add Hebrew 13
    Note that this module is a little bloated already so I understand if you'd like me to publish a module exporting the string "שלוש עשרה" and require that instead.

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function is(x) {
 
         // Languages
         "thirteen", // English
+        "שלוש עשרה", // Hebrew 
         "тринадцать", // Russia
         "trzynaście", // Polish
         "trece", // Spanish
