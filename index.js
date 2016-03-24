6fa58aac7dc72d0918be4e444e9cdc33bccc0343
Marco de Jongh <m.jongh88@gmail.com>
2016-03-24 17:20:14 +1100

    Update index.js
    Gotta make sure we check for lowercase thirteen

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function isThirteen(n, strictEquality) {
   // plz make way for Count Count.
   if (n === 'https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/s320x320/e35/12237511_444845689040315_1101385461_n.jpg') return true;
 
-  if (n === "Thirteen") return true;
+  if (typeof n === 'string' && n.toLowerCase() === "thirteen".toLowerCase()) return true;
   return (strictEquality === true) ? n === 13 : n == 13;
 }
 
