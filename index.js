a131a215721b82d8a2b06828f2a14d457fa434ff
Jezen Thomas <jezenthomas@gmail.com>
2016-03-24 02:06:30 +0100

    Add relevant attributions

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 
 /**
- * @param n {number} The number to compare
+ * @param n {number} The number to compare but also sometimes not a number but not not !NaN
  * @param strictEquality {boolean} Whether strict equality should be used
  * @returns {boolean}
  */
 function isThirteen(n, strictEquality) {
+
+  // plz make way for Count Count.
+  if (n === 'https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/s320x320/e35/12237511_444845689040315_1101385461_n.jpg') return true;
+
   if (n === "Thirteen") return true;
   return (strictEquality === true) ? n === 13 : n == 13;
 }
