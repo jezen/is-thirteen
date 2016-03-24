659550faf5d511039faa1b75b28e32e196457b6a
Ofek Azulay <ofek.azulay@gmail.com>
2016-03-24 15:53:05 +0100

    Added tests for Hebrew and German

--- test.js
+++ test.js
@@ -9,6 +9,8 @@ tap.equal(isThirteen('13', false), true);
 
 tap.equal(isThirteen('thirteen'), true);
 tap.equal(isThirteen('Thirteen'), true);
+tap.equal(isThirteen('dreizehn'), true);
+tap.equal(isThirteen('שלוש עשרה'), true);
 tap.equal(isThirteen('Remy Hadley'), true);
 
 tap.equal(isThirteen('https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/s320x320/e35/12237511_444845689040315_1101385461_n.jpg'), true);
