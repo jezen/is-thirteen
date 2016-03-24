553d3b1bb4987c8b2816b2bceefd3bcce9e3ec4f
Dimasdanz <dimasdanz@gmail.com>
2016-03-24 17:18:51 +0700

    Add binary test

--- test.js
+++ test.js
@@ -12,3 +12,6 @@ tap.equal(isThirteen('Thirteen'), true);
 tap.equal(isThirteen('Remy Hadley'), true);
 
 tap.equal(isThirteen('https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/s320x320/e35/12237511_444845689040315_1101385461_n.jpg'), true);
+
+tap.equal(isThirteen(1101), true);
+tap.equal(isThirteen('1101'), true);
