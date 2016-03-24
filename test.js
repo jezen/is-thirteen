99a381a71119c18570f40f7e782efcf0b7d81c38
Mat Gadd <mat@whiteoctober.co.uk>
2016-03-24 11:42:01 +0000

    Update to use correct syntax

--- test.js
+++ test.js
@@ -2,17 +2,17 @@ const tap = require('tap');
 const isThirteen = require('./');
 
 
-tap.equal(isThirteen(13), true);
+tap.equal(isThirteen(13).thirteen(), true);
 
-tap.equal(isThirteen('13', true), false);
-tap.equal(isThirteen('13', false), true);
+tap.equal(isThirteen('13', true).thirteen(), false);
+tap.equal(isThirteen('13', false).thirteen(), true);
 
-tap.equal(isThirteen('thirteen'), true);
-tap.equal(isThirteen('Thirteen'), true);
-tap.equal(isThirteen('Remy Hadley'), true);
+tap.equal(isThirteen('thirteen').thirteen(), true);
+tap.equal(isThirteen('Thirteen').thirteen(), true);
+tap.equal(isThirteen('Remy Hadley').thirteen(), true);
 
-tap.equal(isThirteen('https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/s320x320/e35/12237511_444845689040315_1101385461_n.jpg'), true);
-tap.equal(isThirteen('http://www.metal-archives.com/images/1/5/3/7/153772.jpg'), false);
+tap.equal(isThirteen('https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/s320x320/e35/12237511_444845689040315_1101385461_n.jpg').thirteen(), true);
+tap.equal(isThirteen('http://www.metal-archives.com/images/1/5/3/7/153772.jpg').thirteen(), false);
 
-tap.equal(isThirteen(1101), true);
-tap.equal(isThirteen('1101'), true);
+tap.equal(isThirteen(1101).thirteen(), true);
+tap.equal(isThirteen('1101').thirteen(), true);
