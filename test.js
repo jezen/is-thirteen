75d499003fd910db49b41cb82325b7f3475976e0
Ola Stensson <ola.stensson@mobenga.com>
2016-03-24 16:34:36 +0100

    Add baker's dozen

--- test.js
+++ test.js
@@ -13,6 +13,7 @@ tap.equal(isThirteen('dreizehn'), true);
 tap.equal(isThirteen('שלוש עשרה'), true);
 tap.equal(isThirteen('Remy Hadley'), true);
 tap.equal(isThirteen('Olivia Wilde'), true);
+tap.equal(isThirteen("baker's dozen"), true);
 
 tap.equal(isThirteen('https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/s320x320/e35/12237511_444845689040315_1101385461_n.jpg'), true);
 tap.equal(isThirteen('http://www.metal-archives.com/images/1/5/3/7/153772.jpg'), false);
