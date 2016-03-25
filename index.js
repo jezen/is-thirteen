2c7ed2eb66adbe9181ee47e1b553384f1e559573
Raphael Moser <raphael.moser@relational.ch>
2016-03-25 14:56:39 +0100

    Support for Swiss German

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function is(x) {
         "kolmetoista", // Finnish
         "treize", // French
         "dreizehn", // German
+        "drizäh", // Swiss German
       "שלוש עשרה", // Hebrew
       "तेरह", //Hindi
         "tizenhárom", // Hungarian
