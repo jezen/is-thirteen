579fc843bc23b143274e20894f401428f71f4603
Gautam krishna.R <rgautamkrishna@gmail.com>
2016-03-27 04:22:00 +0530

    Update test.js

--- test.js
+++ test.js
@@ -93,6 +93,7 @@ tap.equal(is("I3").thirteen(),true); //I3 looks like 13
 tap.equal(is("l3").thirteen(),true); //l3 looks like 13
 tap.equal(is("L3").thirteen(),true); //l3 looks like 13 when lower case
 tap.equal(is("|3").thirteen(),true); //|3 looks like 13
+tap.equal(is("!3").thirteen(),true); //!3 looks like 13
 
 
 
