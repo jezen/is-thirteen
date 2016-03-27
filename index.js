7d23bd8a33e4610a8c46d50415bdeddf356a7b85
Cedric Zirtacic <cicatriz.r00t@gmail.com>
2016-03-27 17:25:00 +0200

    Add function to check functions
    Just check if function() is thirteen or not.

--- index.js
+++ index.js
@@ -169,4 +169,16 @@ function is(x) {
     }
 }
 
+/**
+ * @param f {function} The function to check if it's thirteen
+ * @returns {object}
+ */
+function isFunction(f){
+  if(typeof(f) != "function")
+    return undefined;
+    
+  return is(f.name);
+}
+
 module.exports = is;
+module.exports.isFunction = isFunction;
