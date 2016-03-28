27ca4d3f1144ead4ecb3eb09821fbea521636487
Rafal Wilinski <raf.wilinski@gmail.com>
2016-03-28 12:39:27 +0200

    Introduce coprime/relative integer
    Scientific background: https://en.wikipedia.org/wiki/Coprime_integers
    
    In my humble opinion lack of such important function in this library is kind of misunderstanding. I cannot imagine how much frustration may it cause to current and future users. I hope this will help. 
    
    Regards, Rafal

--- index.js
+++ index.js
@@ -150,6 +150,15 @@ function is(x) {
                 }
             }
         },
+        relatively: {
+            prime: {
+                to: {
+                    thirteen: function() {
+                        return x % 13 !== 0;
+                    }
+                }
+            }
+        },
         greater: {
             than: {
                 thirteen: function() {
