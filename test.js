2011b9f1ee0798a1e48002b1cff5b512a25e59b3
Matheus Felipe <mfelipe@paybyphone.com>
2016-03-29 14:18:50 -0700

    Adds test to confirm that Dilma is 13

--- test.js
+++ test.js
@@ -127,5 +127,5 @@ tap.equal(is("l3").thirteen(),true); //l3 looks like 13
 tap.equal(is("L3").thirteen(),true); //l3 looks like 13 when lower case
 tap.equal(is("|3").thirteen(),true); //|3 looks like 13
 
-
+tap.equal(is("Dilma").thirteen(), true); // Because the supreme Queen of Brazil is 13, amigo
 
