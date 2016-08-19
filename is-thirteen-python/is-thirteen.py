a26fb57d51a7858b40e020bd99f5da4d40772375
John Leonardo <jdleonardo5@icloud.com>
2016-08-19 15:35:34 -0700

    first function

--- is-thirteen-python/is-thirteen.py
+++ is-thirteen-python/is-thirteen.py
@@ -1,1 +1,26 @@
+##CLASS
+class isThirteen:
 
+    def isThirteen(n):
+        ## first checks if int
+        if isinstance(n, int) == True:
+            if n == 13:
+                return True
+            else:
+                return False
+
+        ## then checks if float
+        elif isinstance(n, float) == True:
+            if n == 13.0:
+                return True
+            else:
+                return False
+
+        ## then checks if str, and lowers it
+        elif isinstance(n, str) == True:
+            if n.lower() == "thirteen":
+                return True
+            else:
+                return False
+        else:
+            return False
