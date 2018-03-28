28b3d413aa36174db4127ac30401dc2cd1ed6474
Jinhui Zhang <zjh0930@gmail.com>
2018-03-28 16:52:38 -0700

    added 燕十三

--- consts.js
+++ consts.js
@@ -18,6 +18,7 @@ var thirteenStrings = [
     "https://s3.amazonaws.com/rapgenius/calle13.jpg", // Calle 13, famous Puerto Rican band
     "jason fly", // XIII of The XX
     "http://www.imdb.com/title/tt0798817/", // 13 (2010)
+    "燕十三", // famous charater in movie 《Sword Master》, his name is number 13
 
     "dilma", //Dilma, former president of Brazil. Her number is 13: https://www.google.com/search?q=dilma+13
     "PT", // PT is Brazilian political party represented by the number 13
