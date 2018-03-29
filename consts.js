fa326e706d0301c1ee0c875aa994109521054df0
Ross Olson <Ross@rossolson.com>
2018-03-29 09:57:14 -0700

    Obvious omission submission
    ROT13.

--- consts.js
+++ consts.js
@@ -82,6 +82,11 @@ var thirteenStrings = [
     "- .... .. .-. - . . -.",
     "- .... .. .-. - . . -.",
 
+    // ROT13
+    "guvegrra",
+    "Guvegrra",
+    "GUVEGRRA",
+
     // Caesar shift
     "wkluwhhq",
     "Wkluwhhq",
