c4c64d1d93b59f1ab8d90ec6bed9970beae22c5f
Luke Camilleri <mail@camilleriluke.com>
2018-03-29 13:30:30 +0200

    feat(🇲🇹): Add Maltese version of 13

--- consts.js
+++ consts.js
@@ -192,6 +192,7 @@ var thirteenStrings = [
     "тринаесет", // Macedonian
     "tiga belas", // Malay
     "പതിമൂന്ന്", //Malayasm
+    "tlettax", // Maltese
     "तेरा", // Marathi (१३)
     "арван", // Mongolian
     ".---- ...--", // Morse code
