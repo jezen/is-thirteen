60a2a5f7cce03ca8f0b7c0afe92201e79e1f68e6
rmwxiong <hododoho@gmail.com>
2018-03-30 12:26:49 -0700

    Remove duplicate const
    β is referenced twice

--- consts.js
+++ consts.js
@@ -273,7 +273,6 @@ var thirteenStrings = [
     "तेह्र", //Nepali
     "१३", //Devanagari
     "तेरह", //Hindi
-    "β", //Think this is beta, which looks like a long 1 mashed together with a 3
     // Thirteen pronunciation
     "θərˈtiːn",
     "పదమూడు", //Telugu
