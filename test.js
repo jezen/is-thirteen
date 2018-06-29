d1d5aef17e5a1d678bbabf74a1065b1884879d1f
Cosas de Puma <kikefontanlorenzo@gmail.com>
2018-06-29 23:42:32 +0200

    Hopefully it works!

--- test.js
+++ test.js
@@ -27,6 +27,7 @@ tap.equal(is("Patty Tsai").thirteen(), true);
 tap.equal(is("PT").thirteen(), true);
 tap.equal(is("Washington Luís").thirteen(), true);
 tap.equal(is("Millard Fillmore").thirteen(), true);
+tap.equal(is("13 Reasons Why").thirteen(), true);
 //year of birth test
 tap.equal(is("2003").yearOfBirth(), false);
 
