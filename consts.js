43ddd588c416a794afda054282af7e87cd64ff6f
nikolas <nnyby@columbia.edu>
2018-08-29 12:35:01 -0400

    consts.js: Add the 2003 movie Thirteen
    This is a good movie

--- consts.js
+++ consts.js
@@ -18,6 +18,7 @@ var thirteenStrings = [
     "patty tsai", // 蔡依珊 is a public figure in Taiwan. Her Chinese name sounds like "13".
     "https://s3.amazonaws.com/rapgenius/calle13.jpg", // Calle 13, famous Puerto Rican band
     "jason fly", // XIII of The XX
+    "https://www.imdb.com/title/tt0328538/", // Thirteen (2003)
     "http://www.imdb.com/title/tt0798817/", // 13 (2010)
 
     "dilma", //Dilma, former president of Brazil. Her number is 13: https://www.google.com/search?q=dilma+13
