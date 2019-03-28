fb8263adc73eb95611e0c925a51c1d398c1ca947
Jon Linnell <linnell.jon@gmail.com>
2019-03-28 20:59:13 +0000

    Add isiXhosa

--- consts.js
+++ consts.js
@@ -302,7 +302,8 @@ var thirteenStrings = [
     "онүш", // Kazakh
     "онуш", // Kazakh
     "onúsh", // Kazakh latin
-    "ishumi nantathu" // isiZulu
+    "ishumi nantathu", // isiZulu
+    "lishumi elinesithathu" // isiXhosa
 ];
 
 module.exports = {
