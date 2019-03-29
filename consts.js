1039390454629b7b2920cbf2e16b6516b99a6f94
Celso Fabri Junior <celso.fabri@live.com>
2019-03-29 17:12:05 -0300

    Added Lula support

--- consts.js
+++ consts.js
@@ -20,6 +20,8 @@ var thirteenStrings = [
     "jason fly", // XIII of The XX
     "http://www.imdb.com/title/tt0798817/", // 13 (2010)
     "https://en.wikipedia.org/wiki/XIII_(video_game)", // Because video games are also culture
+    "lula", //Lula, former president of Brazil. His number is 13: https://www.google.com/search?q=lula+13
+    "lula-livre", //Lula, former president of Brazil. His number is 13: https://www.google.com/search?q=lula+13
     "dilma", //Dilma, former president of Brazil. Her number is 13: https://www.google.com/search?q=dilma+13
     "PT", // PT is Brazilian political party represented by the number 13
     "Washington Luís", // Brazil's thirteenth president
