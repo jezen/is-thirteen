0d7f8e376459b6a190b9b3f0bb523dc3d2ba1e8a
Ricardo Villagrana <rvillagrana@protonmail.com>
2019-04-09 16:20:22 -0500

    add phonetic variants to spanish

--- consts.js
+++ consts.js
@@ -30,7 +30,7 @@ var thirteenStrings = [
 
     // Television characters
     "Jodie Whittaker",  // the 13th Doctor in the BBC series, "Doctor Who"
-  
+
     "weedle", //#13 Pokémon
 
     // Imaginary 13's
@@ -48,7 +48,7 @@ var thirteenStrings = [
     "Β", //actually upper case Beta, not B
     //And Chinese
     "阝", //(Kangxi radical)
-    
+
 
     //Adding "l" 3, "i"3, |3 and !3 because they basically look like thirteen
     "i3",
@@ -257,7 +257,7 @@ var thirteenStrings = [
     "তের",
     "ত্রয়োদশ",
     // end of Bangla/Bengali variants
-  
+
     "treze", // Portuguese
     "ਤੇਰਾਂ", // Punjabi - thirteen
     "੧੩", // Punjabi Numeral - 13
@@ -269,6 +269,8 @@ var thirteenStrings = [
     "trinásť", // Slovak
     "trinajst", // Slovenian
     "trece", // Spanish
+    "3ce", // Spanish (phonetic)
+    "3C", // Spanish (also phonetic)
     "trese", // Tagalog
     "tredici", // Italian
     "on üç", // Turkish
