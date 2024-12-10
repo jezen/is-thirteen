f0f513980945f558e98b9b5aae23c6ab2f89af99
Roberto Gigli <47348935+roberto-gigli@users.noreply.github.com>
2024-12-10 14:56:21 +0100

    Update consts.js
    Added Thirteenth zodiac sign

--- consts.js
+++ consts.js
@@ -30,6 +30,7 @@ var thirteenStrings = [
     "Millard Fillmore", // Thirteenth President of the United States
     "Louis XIII", // Thirteenth king of France
     "https://s3.amazonaws.com/rapgenius/calle13.jpg", // Calle 13, famous latin american band
+    "ophiuchus" // Thirteenth zodiac sign
 
 
 
